89579782a5109d6fdc1592cfc72f5d2363d33dbc
sateeshreddy.patil <sateeshreddy.patil@10.96.114.150>
2016-10-26 12:24:21 +0530

    Second Changes

--- src/com/thermofisher/sample/Sample.java
+++ src/com/thermofisher/sample/Sample.java
@@ -3,7 +3,6 @@ package com.thermofisher.sample;
 public class Sample {
 	Sample(){
 		System.out.println("Hai");
-		System.out.println("Hai");
 	}
 
 }
--- src/com/thermofisher/sample/Test.java
+++ src/com/thermofisher/sample/Test.java
@@ -6,6 +6,7 @@ public class Test {
 		System.out.println("Hai");
 		System.out.println("Hai");
 		System.out.println("Hai");
+		System.out.println("Hai");
 	}
 
 }
